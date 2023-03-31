5b974d2d2f43741737f2bdef4ba95d74d98495df
ZohaibBlockchain <113122909+ZohaibBlockchain@users.noreply.github.com>
2023-03-31 21:11:50 +0400

    minor

--- trade.js
+++ trade.js
@@ -32,39 +32,39 @@ export async function main(signal) {
 
 
 
-  if (NewLeverage['leverage'] == leverage) {//Now leverage is
-    if (userData.currentTrade) {
-      if (userData.side == signal) {
-        console.log('Same Direction');
-        return;
-      }
+  // if (NewLeverage['leverage'] == leverage) {//Now leverage is
+  //   if (userData.currentTrade) {
+  //     if (userData.side == signal) {
+  //       console.log('Same Direction');
+  //       return;
+  //     }
 
-      let res = await settlePreviousTrade();
-      console.log(res);
-      if (res['symbol'] == userData.symbol) {
-        //Now create new Order
-        userData.currentTrade = false;
-        let res = await CreateNewTrade(signal);
-        if (res['symbol'] == userData.symbol) {
-          userData.currentTrade = true;
-          userData.side = signal;
-          console.log(res);
-        } else {
-          console.log('Error While putting a trade...')
-        }
-      }
-    }
-    else {
-      let res = await CreateNewTrade(signal);
-      console.log(res);
-      if (res['symbol'] == userData.symbol) {
-        userData.currentTrade = true;
-        userData.side = signal;
-      } else {
-        console.log('Error While putting a trade...')
-      }
-    }
-  }
+  //     let res = await settlePreviousTrade();
+  //     console.log(res);
+  //     if (res['symbol'] == userData.symbol) {
+  //       //Now create new Order
+  //       userData.currentTrade = false;
+  //       let res = await CreateNewTrade(signal);
+  //       if (res['symbol'] == userData.symbol) {
+  //         userData.currentTrade = true;
+  //         userData.side = signal;
+  //         console.log(res);
+  //       } else {
+  //         console.log('Error While putting a trade...')
+  //       }
+  //     }
+  //   }
+  //   else {
+  //     let res = await CreateNewTrade(signal);
+  //     console.log(res);
+  //     if (res['symbol'] == userData.symbol) {
+  //       userData.currentTrade = true;
+  //       userData.side = signal;
+  //     } else {
+  //       console.log('Error While putting a trade...')
+  //     }
+  //   }
+  // }
 }
 
 
