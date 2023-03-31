1f7d59f46a7daac296a6fdf95a1a4f4b3cf4add8
ZohaibBlockchain <113122909+ZohaibBlockchain@users.noreply.github.com>
2023-03-31 20:52:52 +0400

    fix issues

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 import {main} from './trade.js';
 
+
+
 import bodyParser from 'body-parser';
 app.use(bodyParser.json());
 // app.use(json());
--- trade.js
+++ trade.js
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
-dotenv.config
-
+dotenv.config();
 import Binance from 'node-binance-api';
+
+
 
 
 const binance = new Binance().options({
@@ -13,9 +14,10 @@ const binance = new Binance().options({
 const leverage = 2;
 const userData = {
   symbol: 'BTCUSDT',
-  side:'long',
-  quantity:0.001,
-  currentTrade:false
+  side: 'long',
+  quantity: 0.001,
+
+  currentTrade: false
 };
 
 
@@ -23,39 +25,42 @@ const userData = {
 
 
 export async function main(signal) {
-   console.info(signal);
-  let NewLeverage  = await setleverage();
+  console.info('signal ', signal);
+  let NewLeverage = await setleverage();
+  console.log(userData.side);
+  console.log(NewLeverage['leverage']);
+
+
+
   if (NewLeverage['leverage'] == leverage) {//Now leverage is
-    if(userData.currentTrade)
-    {
-      if(userData.side == signal)
-      {
+    if (userData.currentTrade) {
+      if (userData.side == signal) {
         console.log('Same Direction');
         return;
       }
 
       let res = await settlePreviousTrade();
-      if(res.status == 'FILLED')
-      {
+      console.log(res);
+      if (res['symbol'] == userData.symbol) {
         //Now create new Order
-        userData.currentTrade=false;
+        userData.currentTrade = false;
         let res = await CreateNewTrade(signal);
-        if(res.status == 'FILLED')
-        {
+        if (res['symbol'] == userData.symbol) {
           userData.currentTrade = true;
           userData.side = signal;
-        }else{
+          console.log(res);
+        } else {
           console.log('Error While putting a trade...')
         }
       }
     }
-    else{
+    else {
       let res = await CreateNewTrade(signal);
-      if(res.status == 'FILLED')
-      {
+      console.log(res);
+      if (res['symbol'] == userData.symbol) {
         userData.currentTrade = true;
         userData.side = signal;
-      }else{
+      } else {
         console.log('Error While putting a trade...')
       }
     }
@@ -77,27 +82,24 @@ async function setleverage() {
 
 }
 
-async function settlePreviousTrade()
-{
+async function settlePreviousTrade() {
   return new Promise(async (resolve, reject) => {
-  if(userData.side == 'long')
-  {
-   resolve(await binance.futuresMarketSell(userData.symbol,userData.quantity)); 
-  }else{
-   resolve(await binance.futuresMarketBuy(userData.symbol,userData.quantity));
-  }
-});
+    if (userData.side == 'long') {
+      resolve(await binance.futuresMarketSell(userData.symbol, userData.quantity));
+    } else {
+      resolve(await binance.futuresMarketBuy(userData.symbol, userData.quantity));
+    }
+  });
 }
 
 
-async function CreateNewTrade(signal)
-{
+async function CreateNewTrade(signal) {
   return new Promise(async (resolve, reject) => {
-  if(signal == 'long')
-  { resolve(await binance.futuresMarketBuy(userData.symbol,userData.quantity));
-  }else{
-    resolve(await binance.futuresMarketSell(userData.symbol,userData.quantity)); 
-  }
-});
+    if (signal == 'long') {
+      resolve(await binance.futuresMarketBuy(userData.symbol, userData.quantity));
+    } else {
+      resolve(await binance.futuresMarketSell(userData.symbol, userData.quantity));
+    }
+  });
 }
 
